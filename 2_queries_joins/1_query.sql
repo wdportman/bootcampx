a6d8c0fcec9855871f4956ee13f41b425be3e84d
Will <wdportman@gmail.com>
2020-10-12 22:20:07 +0000

    Write two more joins queries

---
+++ 2_queries_joins/1_query.sql
@@ -0,0 +1,3 @@
+SELECT sum(duration)
+FROM assignment_submissions JOIN students ON student_id = students.id
+WHERE students.name = 'Ibrahim Schimmel'
---
+++ 2_queries_joins/2_query.sql
@@ -0,0 +1,11 @@
+SELECT sum(assignment_submissions.duration) as total_duration
+FROM assignment_submissions
+JOIN students ON assignment_submissions.student_id = students.id
+JOIN cohorts ON students.cohort_id = cohorts.id
+WHERE cohorts.name = 'FEB12'
+
+-- SELECT sum(assignment_submissions.duration) as total_duration
+-- FROM assignment_submissions
+-- JOIN students ON students.id = student_id
+-- JOIN cohorts ON cohorts.id = cohort_id
+-- WHERE cohorts.name = 'FEB12';
